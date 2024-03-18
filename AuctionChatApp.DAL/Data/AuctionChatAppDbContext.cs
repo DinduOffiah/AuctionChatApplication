5862ea1b20d13371bcdbce27c92526abe6a076aa
Chidindu <chidinduoffiah@gmail.com>
2024-03-18 23:40:47 +0100

    Added DbContext

---
+++ AuctionChatApp.DAL/Data/AuctionChatAppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using AuctionChatApp.DAL.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuctionChatApp.DAL.Data
+{
+    public class AuctionChatAppDbContext : DbContext
+    {
+        public AuctionChatAppDbContext(DbContextOptions<AuctionChatAppDbContext> options)
+          : base(options)
+        {
+        }
+
+        public DbSet<AuctionItem> AuctionItems { get; set; }
+        public DbSet<Bid> Bids { get; set; }
+        public DbSet<AuctionItem> Invoices { get; set; }
+        public DbSet<User> Users { get; set; }
+    }
+}
