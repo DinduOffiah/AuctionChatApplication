6568b687d30ad4a4538a6fee8f8c8169ea5ef29c
Chidindu <chidinduoffiah@gmail.com>
2024-03-19 17:25:26 +0100

    Added DTO for AuctionItem

---
+++ AuctionChatApp.Core/DTOs/AuctionItemDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionChatApp.Core.DTOs
+{
+    // DTO for creating an item
+    public class CreateAuctionItemDto
+    {
+        public string? ItemName { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+    }
+
+    // DTO for reading an item
+    public class ReadAuctionItemDto
+    {
+        public string? ItemName { get; set; }
+        public string? Description { get; set; }
+        public int Quantity { get; set; }
+        public string? ItemUniqueNumber { get; set; }
+    }
+}
