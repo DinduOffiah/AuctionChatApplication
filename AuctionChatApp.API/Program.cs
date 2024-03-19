ae641e6f45ebb074770368bc823c92a9bcc273e5
Chidindu <chidinduoffiah@gmail.com>
2024-03-19 17:38:41 +0100

    Configured Database Context in Program File

--- AuctionChatApp.API/Program.cs
+++ AuctionChatApp.API/Program.cs
@@ -1,6 +1,16 @@
+using AuctionChatApp.Core.Interfaces;
+using AuctionChatApp.Core.Services;
+using AuctionChatApp.DAL.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddScoped<IAuctionItem, AuctionItemService>();
+
+// Configure the database context and connection string
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AuctionChatAppDbContext>(options => options.UseSqlServer(connectionString, x => x.MigrationsAssembly("AuctionChatApp.DAL")).EnableDetailedErrors(true));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
