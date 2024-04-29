73a76cb6ae6e3a1b2b11a2bf920308521fa3a008
Chidindu <chidinduoffiah@gmail.com>
2024-04-29 12:06:34 +0100

    Added Base Entity

---
+++ AuctionChatApp.DAL/Models/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionChatApp.DAL.Models
+{
+    public class BaseEntity
+    {
+        public DateTime? CreatedDate { get; set; } = DateTime.Now;
+        public DateTime? LastModifiedDate { get; set; } = DateTime.Now;
+        public bool? IsDeleted { get; set; } = false;
+    }
+}
--- AuctionChatApp.DAL/Models/User.cs
+++ AuctionChatApp.DAL/Models/User.cs
@@ -8,11 +8,6 @@ namespace AuctionChatApp.DAL.Models
 {
     public class User
     {
-        public int Id { get; set; }
         public string? Username { get; set; }
-        public string? Email { get; set; }
-        public DateTime? CreatedDate { get; set; } = DateTime.Now;
-        public DateTime? LastModifiedDate { get; set; } = DateTime.Now;
-        public bool? IsDeleted { get; set; } = false;
     }
 }
