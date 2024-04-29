466050b4b0b9fe0e6cbda7b362365f40facf140c
Chidindu <chidinduoffiah@gmail.com>
2024-04-29 13:14:23 +0100

    Added Database Initializer

---
+++ AuctionChatApp.DAL/Data/DatabaseInitializer.cs
@@ -0,0 +1,28 @@
+﻿using AuctionChatApp.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionChatApp.DAL.Data
+{
+    public static class DatabaseInitializer
+    {
+        public static async Task InitializeAsync(UserManager<User> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            var roles = new List<string> { "User", "Admin" };
+
+            foreach (var role in roles)
+            {
+                var roleExist = await roleManager.RoleExistsAsync(role);
+
+                if (!roleExist)
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+        }
+    }
+}
