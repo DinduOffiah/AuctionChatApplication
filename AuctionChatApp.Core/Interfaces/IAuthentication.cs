61527af48c613235c7a5cf032bea513861ff5643
Chidindu <chidinduoffiah@gmail.com>
2024-04-29 13:55:50 +0100

    Added IAuthentication Interface

---
+++ AuctionChatApp.Core/Interfaces/IAuthentication.cs
@@ -0,0 +1,18 @@
+﻿using AuctionChatApp.DAL.Models;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AuctionChatApp.Core.Interfaces
+{
+    public interface IAuthentication
+    {
+        Task<IdentityResult> RegisterUser(RegisterModel model);
+        Task<SignInResult> LoginUser(LoginModel model);
+        Task<IdentityResult> AssignAdmin(string userId);
+        string GenerateJwtToken(User user);
+    }
+}
